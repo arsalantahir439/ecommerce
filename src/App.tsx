16e634341b7830398711511d128048e3d1821c24
arsalan-tahir <arsalan.tahir@ceative.co.uk>
2022-11-14 05:52:08 -0800

    new setup created

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import Home from './components/home/home';
+import './scss/common.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Box sx={{ background: 'red', p: 1 }}>weerewre</Box>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
